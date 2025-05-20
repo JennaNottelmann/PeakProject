2c9c7dbaad22bb6e0876e695d974a9a072d966a3
JennaNottelmann <j.nottelmann93@gmail.com>
2025-05-20 11:40:56 +0200

    changes

--- backend/auth.py
+++ backend/auth.py
@@ -1,6 +1,10 @@
 import json
+import os
 
-with open("/backend/user_db.json", "r") as f:
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+USER_DB_PATH = os.path.join(BASE_DIR, "user_db.json")
+
+with open(USER_DB_PATH, "r") as f:
     USERS = json.load(f)
 
 def check_login(username, password):
